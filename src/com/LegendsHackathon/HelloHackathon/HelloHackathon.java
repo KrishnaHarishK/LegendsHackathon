0e7723cf10a2465476136df509ad941e7077ad99
KrishnaHarishK <krishnaharish.k@gmail.com>
2015-07-30 01:06:15 -0700

    Initial Checkin

---
+++ src/com/LegendsHackathon/HelloHackathon/HelloHackathon.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.LegendsHackathon.HelloHackathon;
+
+import java.io.*;
+
+/**
+ * @author Administrator
+ *
+ */
+public class HelloHackathon {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("Hello Hackathon");
+	}
+
+}
