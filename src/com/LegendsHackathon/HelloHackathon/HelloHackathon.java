3128ce25e69e83ba1ed1c771349bed4bde9c14f1
KrishnaHarishK <krishnaharish.k@gmail.com>
2015-07-30 02:18:42 -0700

    Added travis.ym; and also changed code to test travis

--- src/com/LegendsHackathon/HelloHackathon/HelloHackathon.java
+++ src/com/LegendsHackathon/HelloHackathon/HelloHackathon.java
@@ -15,6 +15,8 @@ public class HelloHackathon {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println ("Hello Hackathon");
+		
+		System.out.println ("Hackathon is our priced possession!!!");
 	}
 
 }
