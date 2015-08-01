9639742250780e1dc093ff7e9c9fdebb3bdd5a08
KrishnaHarishK <krishnaharish.k@gmail.com>
2015-08-01 02:24:11 -0700

    C

--- src/com/LegendsHackathon/HelloHackathon/HelloHackathon.java
+++ src/com/LegendsHackathon/HelloHackathon/HelloHackathon.java
@@ -15,7 +15,7 @@ public class HelloHackathon {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println ("Hello Hackathon");
-		System.out.println ("Hackathon is our priced possession!!!");
+		//System.out.println ("Hackathon is our priced possession!!!");
 	}
 
 }
